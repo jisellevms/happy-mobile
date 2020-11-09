e96f37da5dce577aff75c022a010d4be01590a27
Jiselle Martins <jisellevms@gmail.com>
2020-11-09 12:56:51 -0300

    adicionando marcação e botão

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE, Callout } from 'react-native-maps';
 import mapMarker from './src/images/map-marker.png';
+import { Feather} from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -14,20 +15,32 @@ export default function App() {
           longitude: -38.5163695,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
-        }} />
+        }}>
       <Marker
         icon={mapMarker}
         coordinate={{
           latitude: -3.7268249,
           longitude: -38.5163695,
         }}
+        calloutAnchor={{
+          x: 2.7,
+          y: 0.8
+        }}
       >
-        <Callout>
-          <Text>
-            Lar das criancinhas
-          </Text>
+        <Callout tooltip onPress={()=>{}}>
+          <View style={styles.calloutContainer}>
+            <Text style={styles.calloutText}>Lar das criancinhas</Text>
+          </View>
         </Callout>
       </Marker>
+      </MapView>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+          <TouchableOpacity style={styles.createOrphanagesButton}
+          onPress={() => {}}>
+            <Feather name="plus" size={20} color="#FFF" />
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -39,6 +52,47 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
+  },
+  calloutContainer:{
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14
+  },
 
+  footer:{
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+    
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  footerText:{
+    color: '#8fa7b3'
+  },
+
+  createOrphanagesButton:{
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
